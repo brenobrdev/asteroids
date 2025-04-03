20626ac0e09b2fbd61246ca542e1ea637c620b74
BrenoBrDev <brenobr.dev@gmail.com>
2025-04-03 10:21:41 -0500

    Add delta time logic and player

--- main.py
+++ main.py
@@ -1,20 +1,26 @@
 import pygame
 import constants
+import player as p
 
 def main():
     print("Starting Asteroids!")
     print(f"Screen width: {constants.SCREEN_WIDTH}")
     print(f"Screen height: {constants.SCREEN_HEIGHT}")
     pygame.init()
-    window = pygame.display.set_mode((constants.SCREEN_WIDTH, constants.SCREEN_HEIGHT))
+    screen = pygame.display.set_mode((constants.SCREEN_WIDTH, constants.SCREEN_HEIGHT))
+    clock = pygame.time.Clock()
+    dt = 0
+    player = p.Player(constants.SCREEN_WIDTH/2, constants.SCREEN_HEIGHT/2)
 
     while True:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
         
-        window.fill((0,0,0))
+        screen.fill(pygame.Color("black"))
+        player.draw(screen)
         pygame.display.flip()
+        dt = clock.tick(constants.GAME_FPS)/1000
 
 if __name__ == "__main__":
     main()
